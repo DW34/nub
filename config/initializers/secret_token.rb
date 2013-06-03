59b3a7b879346000d2622eab6fe46cfadc67ac61
Stef Lewandowski <stef@stef.io>
2013-06-03 13:36:04 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tentoring::Application.config.secret_token = 'a958105845122ae1d6202bba2766d60f880352fe33d2f22d5755c4eec5f33afd5e52337f49bbb9f7b8c78436b0f84653b479e149a7232a480fd7176c69958c5d'
